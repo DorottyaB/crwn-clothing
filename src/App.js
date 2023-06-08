b27cab9eda80979b03cbbd33585334315844866e
Dorottya <dorottyabalogh89@gmail.com>
2023-06-08 16:08:36 +0200

    improve performance by lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 
 import {
@@ -7,12 +7,15 @@ import {
   createUserDocumentFromAuth,
 } from './utils/firebase/firebase.utils';
 
-import Navigation from './routes/navigation/navigation.component';
-import Home from './routes/home/home.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+import Spinner from './components/spinner/spinner.component';
+
 import { setCurrentUser } from './features/user/userSlice';
+
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,14 +36,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
